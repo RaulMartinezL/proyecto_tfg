aa58c92ade7becf0187433bd538f4091ff92fd21
Raul Martinez Lopez <47757996+RaulMartinezL@users.noreply.github.com>
2020-09-28 19:46:25 +0200

    Update test.py

--- backend/post/test.py
+++ backend/post/test.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 
+from .models import Profile
+from .models import Message
+
 
 class BasicTest(TestCase):
 
@@ -17,6 +20,32 @@ class BasicTest(TestCase):
     def test_basic2(self):
         "Basic2 with setup"
         assert self.a != 2
+        
+     def test_fields_profile(self):
+
+        profile = Profile()
+        profile.literature = "Esto es la biografia del perfil"
+        profile.music = "Me gusta el pop y el popunk"
+        profile.sport = "F1"
+        profile.party = "mode on"
+        profile.art = "calaveras"
+        profile.save()
+
+        record = Profile.get(pk=profile.pk)
+        self.assertEqual(record, profile)
+
+
+
+    def test_fields_message(self):
+
+        message = Message()
+        message.text = "Esto es el cuerpo del mensaje"
+        message.username = "Usuario1"
+        message.date = "05/05/2000"
+        message.save()
+
+        record = Message.get(pk=message.pk)
+        self.assertEqual(record, message)
 
    
 
