82cefc66644021a9b6af490a436ed348b30b1cff
Raul Martinez Lopez <47757996+RaulMartinezL@users.noreply.github.com>
2020-09-28 19:42:05 +0200

    Update settings.py

--- backend/django_redes_sociales/settings.py
+++ backend/django_redes_sociales/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.test.TestCase ',
     'webpack_loader',
     'rest_framework',  # api rest framwework
     'corsheaders',
